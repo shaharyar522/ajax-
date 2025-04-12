064ffc5802abdd5761783964d7ea65bef923077d
shaharyar522 <shaharyar.khan4511@gmail.com>
2025-04-12 20:26:50 +0500

    working on post fetch api random

--- ajax.js
+++ ajax.js
@@ -9,11 +9,11 @@ function buttonClickHanler() {
   //Instantiate  an xhr  object
   const xhr = new XMLHttpRequest();
 
-//   xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+  //   xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
 
-//now this is the post request method 
+  //now this is the post request method
   xhr.open("POST", "https://dummy.restapiexample.com/api/v1/create", true);
-  xhr.getResponseHeader('Content-type', 'application/json');
+  xhr.getResponseHeader("Content-type", "application/json");
 
   //what to do on progresss  (optional)
 
@@ -24,16 +24,47 @@ function buttonClickHanler() {
   xhr.onload = function () {
     if (this.status == 200) {
       console.log(this.responseText);
-    }
-    else
-    {
-        console.log("some error occur");
+    } else {
+      console.log("some error occur");
     }
   };
   // send  the request
- 
 
   params = `{"name":"test112","salary":"123","age":"23"}`;
   xhr.send(params);
   console.log("we are done!");
+}
+
+// https://dummy.restapiexample.com/api/v1/employees
+// now workin on populate button
+let popbtn = document.getElementById("popbtn");
+
+popbtn.addEventListener("click", pophandler);
+
+function pophandler() {
+  console.log(" you have click the btnbutton");
+  //Instantiate  an xhr  object
+  const xhr = new XMLHttpRequest();
+
+  //now this is the post request method
+  xhr.open("GET", "https://dummy.restapiexample.com/api/v1/employees", true);
+
+  //what to do  when  response  is  ready
+  xhr.onload = function () {
+    if (this.status == 200) {
+      let obj = JSON.parse(this.responseText);
+      console.log(obj);
+      let list = document.getElementById("list");
+      str = "";
+      for (key in obj) {
+        str += `<li>${obj[key].employee_name}</li>`;
+      }
+      list.innerHTML = str;
+    } else {
+      console.log("some error occur");
+    }
+  };
+  // send  the request
+  xhr.send();
+  console.log("we are done! fetching the Employee Data");
 }
